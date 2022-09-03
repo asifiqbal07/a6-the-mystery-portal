e923a7528cab002c1af936da145ebe96b02fca75
asifiqbal07 <as.asif.07@gmail.com>
2022-09-04 03:53:04 +0600

    main section completed

--- JS/news.js
+++ JS/news.js
@@ -25,12 +25,15 @@ const loadFullNews = async (id) => {
 }
 
 const displayNews = (data) => {
+  
   const displayNewsField = document.getElementById('news-field');
   displayNewsField.innerHTML = ``;
 
+  
+  
   data.forEach(data => {
     const newsDev = document.createElement('div')
-    newsDev.classList.add('row','g-0','mb-4','p-3','bg-white','shadow-sm');
+    newsDev.classList.add('row', 'g-0', 'mb-4', 'p-3', 'bg-white', 'shadow-sm');
     newsDev.innerHTML = `
                     <div class="col-md-4">
                         <img src="${data.image_url}" class="img-fluid rounded-start " alt="...">
@@ -38,29 +41,51 @@ const displayNews = (data) => {
                     <div class="col-md-8 px-4 py-2">
                         <div class="card-body">
                             <h5 class="card-title text-info">${data.title}</h5>
-                            <p class="card-text py-2 text-truncate">${data.details}</p>
+                            <p class="card-text py-2">${data.details.slice(0, 300)}...</p>
                         </div>
-                        <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between mt-3 mt-lg-5">
+                        <div class="d-flex flex-column flex-lg-row align-items-center justify-content-between mt-1 mt-lg-5">
                           <div class = "d-flex w-25 align-items-center ">
                              <img src="${data.author.img}" class="w-25 rounded-circle" alt="...">
                              <div>
-                               <h6 class="ps-3">${data.author.name}</h6>
+                               <h6 class="ps-3">${data.author.name ? data.author.name : 'Sorry Data Not Found'}</h6>
                              </div>
                           </div>
                            <div class = "d-flex align-items-center">
                            <img src="images/eye.svg" class="img-fluid rounded-start " alt="...">
-                             <h6 class="p-3"> View: ${data.total_view} </h6>
+                             <h6 class="p-3"> View: ${data.total_view ? data.total_view : '0'} </h6>
                            </div>
                            <div>
-                           <button onclick="loadPhoneDetails()" href="#" class="btn btn-info text-white " data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
+                           <button onclick="displayNews(${data.title ? data.title : 'Sorry Data Not Found'})" href="#" class="btn btn-info text-white " data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show More</button>
                         </div>
     `;
     displayNewsField.appendChild(newsDev);
+    
+
+    // Modal Section //
+    const modalTittle = document.getElementById('phoneDetailModalLabel');
+    modalTittle.innerText = data.title;
+    const newsDetails = document.getElementById('news-details');
+    newsDetails.innerHTML = `
+    <img src="${data.image_url}" class="img-fluid rounded-start " alt="...">
+    <h6 class="mt-2"> Rating: ${data.rating.number}, ${data.rating.badge}. </h6>
+    <p> <span class="fw-semibold">Details:</span> ${data.details}</p>
+    <h6> Publish Date: ${data.author.published_date}.</h6>
+    `;
+
   });
 
 }
 
+// const displayShowDetails = data =>{
+
+// }
 
 
 loadFullNews();
 loadTopBars();
+
+// datas.sort((a, b) => {
+//   return b.propertyName - a.propertyName;
+//   });
+
+// objs.sort((a,b) => (a.last_nom > b.last_nom)
